93de9fbb77c642365ae7925cec6380dfca364ead
aliabdulbaqi01 <aliabdulbaqi01@gmail.com>
2024-08-24 14:10:38 +0300

    migrate admin and delete the 4 exist  migration table

--- database/migrations/2024_08_24_100844_create_admins_table.php
+++ database/migrations/2024_08_24_100844_create_admins_table.php
@@ -11,14 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
+            $table->string('username', 100);
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('added_by');
+            $table->integer('updated_by');
+            $table->tinyInteger('active');
+            $table->date('date');
             $table->timestamps();
+            $table->integer('comp_code');
         });
     }
 
@@ -27,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 };
